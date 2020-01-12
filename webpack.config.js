8f5aea336f17af75384759b8af343826858b5dc5
Lahiru Kurukulasuriya <lahirukasun@gmail.com>
2020-01-12 14:54:51 +0800

    file loader dependancies added

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,13 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.js@/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
                 test: /\.html$/,
                 use: [
                     {
@@ -12,6 +19,12 @@ module.exports = {
                     }
                 ]
             },
+            {
+                test: /\.(png|svg||jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
         ]
     },
     plugins: [
